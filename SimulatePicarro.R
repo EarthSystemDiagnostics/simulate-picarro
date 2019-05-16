92c90d4e5aed75a061eb8b13c5d14f477b24fc76
tlaepple <tlaepple@awi.de>
2019-05-16 19:29:51 +0200

    first commit

---
+++ SimulatePicarro.R
@@ -0,0 +1,121 @@
+## Aim: 
+
+
+library(tidyverse)
+
+#Define the machine characteristics (to be determined from experiments)
+SD.MACHINE     <- 0.025 #standard deviation of random measurement error assumed to be iid and gaussian [Value from PICARRO]
+DRIFT.MACHINE  <- 0.2 / 24 / 30 #linear Drift in permille per hour, estimate by hand from the experiment [Value from PICARRO would be 0.2 permille per 24h = 0.2/24]
+MEMORY.MACHINE <- 0.5 #Memory coefficient of the machine
+
+DRIFT.VIAL     <- 0.02 / 100 #Linear Drift in permille per hour and per hole in the Septum
+
+#Holes in the vial are modeled that an untouched vial will first get one hole... and than just an incremental increase of the hole size
+DHOLE.VIAL     <- 0.0 #Not every injection will create a new hole; this coefficient determines the increase of the Septum holes per injection
+DWATER.INJECTION <- 1.9/1000 #Injection volume in ml
+
+TIME.INJECTION <- 9 #Time per injection in minutes
+
+
+##' @title Simple simulation of assumed PICARRO measurement process
+##' @param vial dataframe with columns d18O (in permille),water.volume (in ml),holes (#holes in the septum)
+##' @param sequence vector giving the sequence of the measurements
+##' @param names vector with the same length as sequence, providing names for the blocks; not used in the simulation but gets output in Analysis
+##' @param d18O.machine Initial d18O (permille) in the machine... needed for the drift
+##' @return data.frame  with columns "Line","Analysis","Port","d.18_16.Mean"
+##' @author Thomas Laepple 
+simulate <- function(vial, sequence, names, d18O.machine = -37.5)
+{
+    ##Initial values
+    runtime.minutes <- 0
+    
+    ##Predefine an empty dataframe as output... must me an easier way
+    output <- matrix(NA,length(sequence),4)
+    colnames(output) <- c("Line","Analysis","Port","d.18_16.Mean")
+    output <- as.data.frame(output)
+
+    ##Run the simulation
+    ##i.step = injection step
+    for (i.step in 1:length(sequence))
+    {
+        runtime.minutes = runtime.minutes + TIME.INJECTION
+
+
+        ##Every timestep, all vials drift proportional to the number of holes and the time passed (in hours)
+        vial$d18O<-vial$d18O + vial$holes*DRIFT.VIAL*TIME.INJECTION/60
+        
+
+        
+        ##Choose the vial for sampling
+        i.vial <- sequence[i.step]
+
+        ##remove water from the vial 
+        vial$water.volume[i.vial] <- vial$water.volume[i.vial]-DWATER.INJECTION
+
+
+        ##Put / increase hole in the septum
+        if (vial$holes[i.vial]==0)
+        {
+            vial$holes[i.vial]=1
+        } else vial$holes[i.vial]=vial$holes[i.vial]+DHOLE.VIAL
+
+       
+        
+        ##perform the measurement of d18O
+        measured <- vial$d18O[i.vial] + rnorm(1,sd=SD.MACHINE) + DRIFT.MACHINE*(runtime.minutes/60) - (vial$d18O[i.vial]-d18O.machine) * MEMORY.MACHINE
+
+
+        d18O.machine <- measured
+
+        ##Save the output
+        output$Line[i.step]<-i.step
+        output$Analysis[i.step]<-names[i.step]
+        output$Port[i.step]<-paste(i.vial)
+        output$d.18_16.Mean[i.step]<-measured
+    }
+
+return(output)
+}
+
+
+
+#Define what is initially in the vials; here we use 6 vials with standards
+vial<-data.frame(d18O=c(-38.8,-38.8,-38.8,-38.8,-38.8,-38.8),water.volume=c(1.6,1.6,1.6,1.6,1.6,1.6),holes=c(0,0,0,0,0,0))
+
+##Define the sequence of the measurements... blocks are just used to simplify the construction; block1 is the repeating 75,75,10 injections; block2 three untouched vials at the end to separate the instrument from the vial drift
+
+block1 <-  c(rep(1,75),rep(2,75),rep(3,10))
+block2 <-  c(rep(4,75),rep(5,75),rep(6,75))
+sequence <- c(block1,block1,block1,block1,block1,block2)
+
+#Arbitary naming of blocks that is later used to average the blocks
+names <- (c(block1,block1+3,block1+6,block1+9,block1+12,block2+12))
+
+
+##Run 4 times and put them all in one big dataframe
+run1 <- simulate(vial,sequence,names)
+run2 <- simulate(vial,sequence,names)
+run3 <- simulate(vial,sequence,names)
+run4 <- simulate(vial,sequence,names)
+
+run1$Replicate = 1
+run2$Replicate = 2
+run3$Replicate = 3
+run4$Replicate = 4
+
+runs <- rbind(run1,run2,run3,run4)
+
+##Basic plots and analyses using tidyR / ggplot
+
+ggplot(runs, aes(x=Line,y=d.18_16.Mean,colour=Port)) +  geom_point()  + ggtitle("all measurements") + facet_wrap(~Replicate)
+meanvals <- runs %>% group_by(Analysis,Replicate) %>% summarize(m=mean(d.18_16.Mean),n=n(),se=sd(d.18_16.Mean)/sqrt(n),Port=Port[1])
+meanvals %>% ggplot( aes(x=Analysis,y=m,colour=Port)) +  geom_point() + geom_errorbar(aes(ymin=m-se,ymax=m+se)) +  ggtitle("mean values and standard errors")  + facet_wrap(~Replicate)
+
+
+
+
+
+
+
+
+
